29c699dd18da03d2dda9cfb92070d9853205c1c3
Iain Buclaw <ibuclaw@gdcproject.org>
2020-12-07 01:25:17 +0100

    libphobos: Align interface of gcc.sections.osx with gcc.sections.elf_shared

--- libphobos/libdruntime/core/sys/darwin/execinfo.d
+++ libphobos/libdruntime/core/sys/darwin/execinfo.d
@@ -21,6 +21,11 @@ extern (C):
 nothrow:
 @nogc:
 
-int backtrace(void** buffer, int size);
-char** backtrace_symbols(const(void*)* buffer, int size);
-void backtrace_symbols_fd(const(void*)* buffer, int size, int fd);
+import core.sys.darwin.config;
+
+static if (__traits(getTargetInfo, "osxVersionMin") >= __MAC_10_5)
+{
+    int backtrace(void** buffer, int size);
+    char** backtrace_symbols(const(void*)* buffer, int size);
+    void backtrace_symbols_fd(const(void*)* buffer, int size, int fd);
+}
